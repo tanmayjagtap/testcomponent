8faa21ca9b277c9f287d5cac40da22720a4c48c5
tanmayjagtap <tanmayjagtap27@gmail.com>
2020-12-07 01:19:59 +0530

    Parralax Effect Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import FirstComp from './Components/FirstComp/FirstComp.js';
+import Parallax from './Components/ParallaxScrolling/ParallaxScrollling';
 const App = () => {
   return (
     <div>
       <FirstComp/>
       {/* <----------------Add Components here ------------> */}
+      <Parallax/>
     </div>
   );
 }  
---
+++ src/Components/ParallaxScrolling/ParallaxScrollling.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ParallexScrolling.css';
+const Parallex = () => {
+    return(
+        <div>
+            <div className='margin'></div>
+            <section className='parallax'>
+                <div className='parallax-inner'>
+                    <h1>Parallax Effect</h1>
+                </div>
+            </section>
+            <div className='margin'></div>
+        </div>
+    );
+}
+
+export default Parallex;
