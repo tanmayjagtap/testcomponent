bf1c6e6114121a8f27af814f152b3ae46edfb797
tanmayjagtap <tanmayjagtap27@gmail.com>
2020-12-07 00:03:33 +0530

    let's Get Started,use same format as FirstComp while adding Comps

---
+++ src/Components/FirstComp/FirstComp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FirstComp.css';
+const FirstComp = () =>{
+    return(
+        <div className='firstSection'>
+            <h1>Let's Get Working!</h1>
+            <p>Add Componenets Below!</p>
+        </div>
+    );
+}
+
+export default FirstComp;
