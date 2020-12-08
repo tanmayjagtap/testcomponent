9dd67161202fc548423d1f904a12102a2f90ab95
tanmayjagtap <tanmayjagtap27@gmail.com>
2020-12-08 13:36:15 +0530

    card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './Card.css';
 const Card = () => {
     return(
-        <div>
-                <div className='card'>
+        <div className='card'>
                     <div className='imgBox'>
                         <img src='https://images.unsplash.com/photo-1548013146-72479768bada?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80' alt='Taj Mahal'></img>
                     </div>
@@ -11,7 +10,6 @@ const Card = () => {
                         <h1>Heading</h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
                     </div>
-                </div>                
         </div>
     );
 }
